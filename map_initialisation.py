474f7c7c58254f6688bb758702a455b9366da408
trofi <trofimenko06@gmail.com>
2022-12-28 13:52:28 +0300

    1

--- map_initialisation.py
+++ map_initialisation.py
@@ -10,7 +10,6 @@ HEIGHT = 950
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
 fps = 60
-font = pygame.font.Font('freesansbold.ttf', 20)
 level = copy.deepcopy(boards)
 color = 'blue'
 PI = math.pi
